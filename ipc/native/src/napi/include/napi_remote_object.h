7b473aaaaed497aed01732e4f6356995399fbc42
zhanghaibo <zhanghaibo0@huawei.com>
2022-01-17 21:00:21 +0800

    change header file of napi
    Signed-off-by: zhanghaibo <zhanghaibo0@huawei.com>
    Change-Id: If77c8cc1d977fdfbcc0b1839f6c197a7334cf18f

--- ipc/native/src/napi/include/napi_remote_object.h
+++ ipc/native/src/napi/include/napi_remote_object.h
@@ -17,7 +17,7 @@
 #define NAPI_IPC_OHOS_REMOTE_OBJECT_H
 
 #include "iremote_object.h"
-#include "napi/native_common.h"
+#include "napi/native_api.h"
 #include "napi/native_node_api.h"
 #include "refbase.h"
 
