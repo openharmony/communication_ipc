97684cd103bfe209a4e2983d6a8db41141ddcf62
zhangxiujian <zhangxiujian@huawei.com>
2022-03-10 08:00:09 +0000

    IPC format modification.
    Signed-off-by: zhangxiujian <zhangxiujian@huawei.com>

--- ipc/test/unittest/rpc/samgr/RpcSamgrTest.cpp
+++ ipc/test/unittest/rpc/samgr/RpcSamgrTest.cpp
@@ -115,7 +115,7 @@ int32_t GetRemoteSystemAbility(IpcIo *data, SvcIdentity *sid)
 int32_t RemoteRequest(uint32_t code, IpcIo *data, IpcIo *reply, MessageOption option)
 {
     int32_t result = ERR_NONE;
-    RPC_LOG_INFO("OnRemoteRequest called.... code = %d", code);
+    RPC_LOG_INFO("OnRemoteRequest called.... code = %u", code);
     switch (code) {
         case GET_SYSTEM_ABILITY_TRANSACTION: {
             int32_t saId;
@@ -163,7 +163,7 @@ int32_t RemoteRequest(uint32_t code, IpcIo *data, IpcIo *reply, MessageOption op
             break;
         }
         default:
-            RPC_LOG_ERROR("unknown code %d", code);
+            RPC_LOG_ERROR("unknown code %u", code);
             break;
     }
     return result;
