94fb81bf2d9f16c6d5fab849a484eb00705c1da6
zhangxiujian <zhangxiujian@huawei.com>
2022-03-15 02:23:55 +0000

    【master->release】IPC codex clean同步修改
    Signed-off-by: zhangxiujian <zhangxiujian@huawei.com>

--- ipc/test/rpc/socket_trans/src/rpc_mini_socket_trans.c
+++ ipc/test/rpc/socket_trans/src/rpc_mini_socket_trans.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -275,6 +275,7 @@ static void *HandleSendReply(void *args)
 
 static int32_t Connect(const char *saSessionName, const char *peerDeviceId, void *args)
 {
+    (void)args;
     if (saSessionName == NULL) {
         RPC_LOG_INFO("saSessionName is null");
         return ERR_FAILED;
@@ -336,6 +337,7 @@ static int32_t Connect(const char *saSessionName, const char *peerDeviceId, void
 
 static int32_t Disconnect(int32_t sessionId)
 {
+    (void)sessionId;
     return ERR_NONE;
 }
 
