cf5a73f5fbaa010c438890832eba4020023bb4cd
wanghaoxu <wanghaoxu1@huawei.com>
2022-07-12 01:57:40 +0000

    update the SA remote call interface
    Signed-off-by: wanghaoxu <wanghaoxu1@huawei.com>

--- interfaces/innerkits/libdbinder/include/rpc_system_ability_callback.h
+++ interfaces/innerkits/libdbinder/include/rpc_system_ability_callback.h
@@ -24,12 +24,9 @@ public:
     using OnLoadSystemAbilityComplete = std::func<void(const std::string& srcNetworkId, 
         int32_t systemAbilityId, const sptr<IRemoteObject>& remoteObject)>;
 
-        
     virtual bool LoadSystemAbilityFromRemote(const std::string& srcNetworkId, int32_t systemAbilityId, OnLoadSystemAbilityComplete callback){ return false; }
     virtual sptr<IRemoteObject> GetSystemAbilityFromRemote(int32_t systemAbilityId) = 0;
 
-
-    
     RpcSystemAbilityCallback() = default;
     virtual ~RpcSystemAbilityCallback() = default;
 };
