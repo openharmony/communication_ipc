55eb9597f3ce822f9703f02975ec6b4c621b3151
Yippo <liuyibo12@huawei.com>
2022-10-10 16:51:05 +0800

    Description:ipc/rpc modify some api errorCode and support the async of Messageoption is boolean Feature or Bugfix:ipc/rpc modify some api errorCode and support the async of Messageoption is boolean Binary Source: No Signed-off-by: Yippo <liuyibo12@huawei.com>

--- ipc/native/src/napi/src/napi_message_option.cpp
+++ ipc/native/src/napi/src/napi_message_option.cpp
@@ -62,6 +62,49 @@ napi_value NapiOhosRpcMessageOptionSetFlags(napi_env env, napi_callback_info inf
 }
 
 /*
+ * Get async to ohos.rpc.MessageOption
+ */
+napi_value NapiOhosRpcMessageOptionIsAsync(napi_env env, napi_callback_info info)
+{
+    napi_value thisVar = nullptr;
+    napi_get_cb_info(env, info, nullptr, nullptr, &thisVar, nullptr);
+    NAPI_ASSERT(env, thisVar != nullptr, "failed to get js message option object");
+    MessageOption *option = nullptr;
+    napi_unwrap(env, thisVar, (void **)&option);
+    NAPI_ASSERT(env, option != nullptr, "failed to get native message option");
+    int flags = option->GetFlags();
+    napi_value result = nullptr;
+    napi_status status = napi_get_boolean(env, flags != 0 ? true : false, &result);
+    NAPI_ASSERT(env, status == napi_ok, "failed to create boolean value");
+    return result;
+}
+
+/*
+ * Set async to ohos.rpc.MessageOption
+ */
+napi_value NapiOhosRpcMessageOptionSetAsync(napi_env env, napi_callback_info info)
+{
+    napi_value thisVar = nullptr;
+    size_t argc = 1;
+    napi_value argv[1] = { 0 };
+    napi_get_cb_info(env, info, &argc, argv, &thisVar, nullptr);
+    NAPI_ASSERT(env, thisVar != nullptr, "failed to get js message option object");
+    napi_valuetype valueType = napi_null;
+    napi_typeof(env, argv[0], &valueType);
+    NAPI_ASSERT(env, valueType == napi_number, "type mismatch for parameter 1");
+    bool flags = false;
+    napi_status status = napi_get_value_bool(env, argv[0], &flags);
+    NAPI_ASSERT(env, status == napi_ok, "failed to get boolean value");
+    MessageOption *option = nullptr;
+    napi_unwrap(env, thisVar, (void **)&option);
+    NAPI_ASSERT(env, option != nullptr, "failed to get native message option");
+    option->SetFlags(static_cast<int32_t> (flags));
+    napi_value result = nullptr;
+    napi_get_undefined(env, &result);
+    return result;
+}
+
+/*
  * Get wait time field from ohos.rpc.MessageOption.
  */
 napi_value NapiOhosRpcMessageOptionGetWaittime(napi_env env, napi_callback_info info)
