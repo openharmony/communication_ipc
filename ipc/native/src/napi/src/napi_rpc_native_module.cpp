d9f2a3f5cb497ef1461e085be8cf2dbdcb7d3bfd
Yippo <liuyibo12@huawei.com>
2022-10-26 18:47:20 +0800

    fixed 9737304 from https://gitee.com/yippo/communication_ipc/pulls/421 Description:ipc/rpc adapts to the errorCode enum defined in the JS API Feature or Bugfix:ipc/rpc adapts to the errorCode enum defined in the JS API Binary Source: No
    Signed-off-by: Yippo <liuyibo12@huawei.com>

--- ipc/native/src/napi/src/napi_rpc_native_module.cpp
+++ ipc/native/src/napi/src/napi_rpc_native_module.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -22,6 +22,7 @@
 #include "napi/native_api.h"
 #include "napi/native_node_api.h"
 #include "napi_remote_object.h"
+#include "napi_rpc_error.h"
 
 namespace OHOS {
 EXTERN_C_START
@@ -34,6 +35,7 @@ static napi_value rpcExport(napi_env env, napi_value exports)
     NAPIRemoteObjectExport(env, exports);
     NAPIRemoteProxyExport(env, exports);
     NAPIMessageOptionExport(env, exports);
+    NapiError::NAPIRpcErrorEnumExport(env, exports);
     return exports;
 }
 EXTERN_C_END
