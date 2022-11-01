63df03966eee8561edd4f7fdc6299b6197fd7fa1
wanderer-dl122 <dengliang21@huawei.com>
2022-11-01 18:36:51 +0800

    修改指针地址传递错误
    Signed-off-by: wanderer-dl122 <dengliang21@huawei.com>

--- ipc/native/src/napi/src/napi_message_parcel.cpp
+++ ipc/native/src/napi/src/napi_message_parcel.cpp
@@ -1704,7 +1704,7 @@ napi_value NAPI_MessageParcel::JS_readStringArray(napi_env env, napi_callback_in
     napi_unwrap(env, thisVar, (void **)&napiParcel);
     NAPI_ASSERT(env, napiParcel != nullptr, "napiParcel is null");
 
-    int32_t arrayLength = napiParcel->nativeParcel_->ReadUint32();
+    uint32_t arrayLength = napiParcel->nativeParcel_->ReadUint32();
     if (argc > 0) {
         NAPI_ASSERT(env, argc == 1, "type mismatch for parameter 1");
         CHECK_READ_LENGTH(env, (size_t)arrayLength, BYTE_SIZE_32, napiParcel);
