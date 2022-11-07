eb2bda2fe442a1b2896c1c706f1c85a279098623
18392170496 <magentang3@huawei.com>
2022-11-07 19:28:21 +0800

    fixed cce3d95 from https://gitee.com/ma-gentang/communication_ipc/pulls/455 Description: ipc fuzz test Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source:No Signed-off-by: @ma-gentang <magentang3@huawei.com>
    Signed-off-by: 18392170496 <magentang3@huawei.com>

--- services/dbinder/test/fuzztest/loadsystemabilitycomplete_fuzzer/loadsystemabilitycomplete_fuzzer.cpp
+++ services/dbinder/test/fuzztest/loadsystemabilitycomplete_fuzzer/loadsystemabilitycomplete_fuzzer.cpp
@@ -25,7 +25,7 @@ namespace OHOS {
 
     bool AttachBusNameObjectTest(const uint8_t* data, size_t size)
     {
-        if ((data == nullptr) || (size == 0)) {
+        if ((data == nullptr) || (size < sizeof(int32_t))) {
             return false;
         }
         IRemoteObject *remoteObject = nullptr;
