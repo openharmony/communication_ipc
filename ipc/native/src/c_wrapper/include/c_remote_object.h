f0f73492cadd79972756e88ab73ed04056563a02
fanxiaoyu <fanxiaoyu3@huawei.com>
2022-12-14 09:36:58 +0000

    Add ipc c language interface.
    Signed-off-by: fanxiaoyu <fanxiaoyu3@huawei.com>

---
+++ ipc/native/src/c_wrapper/include/c_remote_object.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IPC_C_REMOTE_OBJECT_H
+#define IPC_C_REMOTE_OBJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#include "c_parcel.h"
+
+struct CRemoteObjectHolder;
+typedef struct CRemoteObjectHolder CRemoteObject;
+
+struct CDeathRecipient;
+typedef struct CDeathRecipient CDeathRecipient;
+
+// Callback as remote stub
+typedef int (*OnRemoteRequestCb)(const CRemoteObject *stub, int code, const CParcel *data, CParcel *reply);
+typedef void (*OnRemoteObjectDestroyCb)(const void *userData);
+// Callback as death recipient
+typedef void (*OnDeathRecipientCb)(const void *userData);
+typedef void (*OnDeathRecipientDestroyCb)(const void *userData);
+
+CRemoteObject *CreateRemoteStub(const char *desc, OnRemoteRequestCb callback,
+    OnRemoteObjectDestroyCb destroy, const void *userData);
+
+void RemoteObjectIncStrongRef(CRemoteObject *object);
+void RemoteObjectDecStrongRef(CRemoteObject *object);
+
+const void *RemoteObjectGetUserData(CRemoteObject *object);
+
+int RemoteObjectSendRequest(const CRemoteObject *object, uint32_t code,
+    const CParcel *data, CParcel *reply, bool isAsync);
+
+// Death Recipient
+CDeathRecipient *CreateDeathRecipient(OnDeathRecipientCb onDeathRecipient,
+    OnDeathRecipientDestroyCb onDestroy, const void *userData);
+void DeathRecipientIncStrongRef(CDeathRecipient *recipient);
+void DeathRecipientDecStrongRef(CDeathRecipient *recipient);
+bool AddDeathRecipient(CRemoteObject *object, CDeathRecipient *recipient);
+bool RemoveDeathRecipient(CRemoteObject *object, CDeathRecipient *recipient);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* IPC_C_REMOTE_OBJECT_H */
