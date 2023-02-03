9665ff403fae247143c6fbf0fffe144b422f24e5
shufewhx <wanghaoxu1@huawei.com>
2023-02-03 02:01:04 +0000

    decoucple librpc.z.so
    Signed-off-by: shufewhx <wanghaoxu1@huawei.com>

---
+++ ipc/native/src/napi_common/include/napi_remote_object_internal.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NAPI_IPC_OHOS_REMOTE_OBJECT_INTERNAL_H
+#define NAPI_IPC_OHOS_REMOTE_OBJECT_INTERNAL_H
+
+#include "ipc_object_stub.h"
+#include "iremote_object.h"
+#include "message_parcel.h"
+#include "message_option.h"
+#include "napi/native_api.h"
+#include "napi_remote_object.h"
+
+namespace OHOS {
+class NAPIRemoteObject : public IPCObjectStub {
+public:
+    NAPIRemoteObject(napi_env env, napi_value thisVar, const std::u16string &descriptor);
+
+    ~NAPIRemoteObject() override;
+
+    bool CheckObjectLegality() const override;
+
+    int GetObjectType() const override;
+
+    int OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option) override;
+
+    napi_ref GetJsObjectRef() const;
+private:
+    napi_env env_ = nullptr;
+    napi_value thisVar_ = nullptr;
+    static napi_value ThenCallback(napi_env env, napi_callback_info info);
+    static napi_value CatchCallback(napi_env env, napi_callback_info info);
+    napi_ref thisVarRef_ = nullptr;
+    struct ThreadLockInfo {
+        std::mutex mutex;
+        std::condition_variable condition;
+        bool ready = false;
+    };
+    struct CallbackParam {
+        napi_env env;
+        napi_ref thisVarRef;
+        uint32_t code;
+        MessageParcel *data;
+        MessageParcel *reply;
+        MessageOption *option;
+        CallingInfo callingInfo;
+        ThreadLockInfo *lockInfo;
+        int result;
+    };
+    int OnJsRemoteRequest(CallbackParam *jsParam);
+};
+
+struct SendRequestParam {
+    sptr<IRemoteObject> target;
+    uint32_t code;
+    std::shared_ptr<MessageParcel> data;
+    std::shared_ptr<MessageParcel> reply;
+    MessageOption &option;
+    napi_async_work asyncWork;
+    napi_deferred deferred;
+    int errCode;
+    napi_ref jsCodeRef;
+    napi_ref jsDataRef;
+    napi_ref jsReplyRef;
+    napi_ref callback;
+    napi_env env;
+    std::string traceValue;
+    int32_t traceId;
+};
+
+napi_value MakeSendRequestResult(SendRequestParam *param);
+} // namespace OHOS
+#endif // NAPI_IPC_OHOS_REMOTE_OBJECT_H
---
+++ ipc/native/src/napi_common/source/napi_remote_object_holder.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "napi_remote_object_holder.h"
+
+#include <string_ex.h>
+
+namespace OHOS {
+NAPIRemoteObjectHolder::NAPIRemoteObjectHolder(napi_env env, const std::u16string &descriptor)
+    : env_(env), descriptor_(descriptor), cachedObject_(nullptr), localInterfaceRef_(nullptr), attachCount_(1)
+{}
+
+NAPIRemoteObjectHolder::~NAPIRemoteObjectHolder()
+{
+    // free the reference of object.
+    cachedObject_ = nullptr;
+    if (localInterfaceRef_ != nullptr) {
+        napi_delete_reference(env_, localInterfaceRef_);
+    }
+}
+
+sptr<NAPIRemoteObject> NAPIRemoteObjectHolder::Get(napi_value jsRemoteObject)
+{
+    std::lock_guard<std::mutex> lockGuard(mutex_);
+    // grab an strong reference to the object,
+    // so it will not be freed util this reference released.
+    sptr<NAPIRemoteObject> remoteObject = nullptr;
+    if (cachedObject_ != nullptr) {
+        remoteObject = cachedObject_;
+    }
+
+    if (remoteObject == nullptr) {
+        remoteObject = new NAPIRemoteObject(env_, jsRemoteObject, descriptor_);
+        cachedObject_ = remoteObject;
+    }
+    return remoteObject;
+}
+
+void NAPIRemoteObjectHolder::Set(sptr<NAPIRemoteObject> object)
+{
+    std::lock_guard<std::mutex> lockGuard(mutex_);
+    cachedObject_ = object;
+}
+
+void NAPIRemoteObjectHolder::attachLocalInterface(napi_value localInterface, std::string &descriptor)
+{
+    if (localInterfaceRef_ != nullptr) {
+        napi_delete_reference(env_, localInterfaceRef_);
+    }
+    napi_create_reference(env_, localInterface, 1, &localInterfaceRef_);
+    descriptor_ = Str8ToStr16(descriptor);
+}
+
+napi_value NAPIRemoteObjectHolder::queryLocalInterface(std::string &descriptor)
+{
+    if (!descriptor_.empty() && strcmp(Str16ToStr8(descriptor_).c_str(), descriptor.c_str()) == 0) {
+        napi_value ret = nullptr;
+        napi_get_reference_value(env_, localInterfaceRef_, &ret);
+        return ret;
+    }
+    napi_value result = nullptr;
+    napi_get_null(env_, &result);
+    return result;
+}
+} // namespace OHOS
