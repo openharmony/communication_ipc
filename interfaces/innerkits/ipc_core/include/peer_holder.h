260eb5fa2e36163cf877aebca9e56d48fa312d1a
l30043718 <lixiaoying25@huawei.com>
2023-04-03 11:34:12 +0800

    Description:add c++ exegesis Feature or Bugfix:add c++ exegesis Binary Source: No
    Signed-off-by: Lixiaoying25 <lixiaoying25@huawei.com>

--- interfaces/innerkits/ipc_core/include/peer_holder.h
+++ interfaces/innerkits/ipc_core/include/peer_holder.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,7 +24,12 @@ protected:
     PeerHolder(const sptr<IRemoteObject> &object);
 
     ~PeerHolder() = default;
-
+    
+    /**
+     * @brief Gets the proxy object.
+     * @return Returns the IRemoteObject pointer object.
+     * @since 9
+     */
     sptr<IRemoteObject> Remote();
 
 private:
--- interfaces/innerkits/libdbinder/include/rpc_system_ability_callback.h
+++ interfaces/innerkits/libdbinder/include/rpc_system_ability_callback.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,7 +21,22 @@
 namespace OHOS {
 class RpcSystemAbilityCallback {
 public:
+
+    /**
+     * @brief Obtains the specified system capability of the remote device.
+     * @param systemAbilityId Indicates system capability ID.
+     * @return Returns the IRemoteObject pointer object.
+     * @since 9
+     */
     virtual sptr<IRemoteObject> GetSystemAbilityFromRemote(int32_t systemAbilityId) = 0;
+
+    /**
+     * @brief Load the specified system capability of the remote device.
+     * @param srcNetworkId Indicates The network ID of the path.
+     * @param systemAbilityId Indicates system capability ID.
+     * @return Returns <b>true</b> if the operation succeeds; returns <b>false</b> otherwise.
+     * @since 9
+     */
     virtual bool LoadSystemAbilityFromRemote(const std::string& srcNetworkId, int32_t systemAbilityId)
     {
         return false;
