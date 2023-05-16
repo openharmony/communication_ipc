96dde5fd0f3a9fc0c2742cd8a9b328d337f304b7
张泊远yg <zhangboyuan1@huawei.com>
2023-05-16 08:46:29 +0000

    update ipc/native/src/core/source/ipc_object_stub.cpp. Signed-off-by: 张泊远yg <zhangboyuan1@huawei.com>
    Signed-off-by: 张泊远yg <zhangboyuan1@huawei.com>

--- ipc/native/src/core/source/ipc_object_stub.cpp
+++ ipc/native/src/core/source/ipc_object_stub.cpp
@@ -314,7 +314,7 @@ void IPCObjectStub::OnLastStrongRef(const void *objectId)
 #ifndef CONFIG_IPC_SINGLE
         // we only need to erase stub index here, commAuth and appInfo
         // has already been removed either in dbinder dec refcount case
-        // or OnSessionClosed, we also remove commAuth and appInfo in case of leak 
+        // or OnSessionClosed, we also remove commAuth and appInfo in case of leak
         current->DetachCommAuthInfoByStub(this);
         uint64_t stubIndex = current->EraseStubIndex(this);
         current->DetachAppInfoToStubIndex(stubIndex);
