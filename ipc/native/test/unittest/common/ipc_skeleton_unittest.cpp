a7a5b7c9c2cf8486429fda36010581c5f62c9f8e
dengyutao <dengyutao2@huawei.com>
2023-08-10 07:59:48 +0000

    BlockUntilThreadAvailable接口用例添加 https://gitee.com/openharmony/communication_ipc/issues/I7S741
    Signed-off-by: dengyutao <dengyutao2@huawei.com>

--- ipc/native/test/unittest/common/ipc_skeleton_unittest.cpp
+++ ipc/native/test/unittest/common/ipc_skeleton_unittest.cpp
@@ -496,6 +496,9 @@ HWTEST_F(IPCSkeletonTest, SetCallingIdentityTest002, TestSize.Level1)
 HWTEST_F(IPCDfxTest, BlockUntilThreadAvailableTest001, TestSize.Level1)
 {
     IPCDfx *ipcDfxTest = new IPCDfx();
+    if (ipcDfxTest == nullptr) {
+        return;
+    }
     IPCProcessSkeleton *current = IPCProcessSkeleton::GetCurrent();
     if (current != nullptr) {
         ipcDfxTest->BlockUntilThreadAvailable();
