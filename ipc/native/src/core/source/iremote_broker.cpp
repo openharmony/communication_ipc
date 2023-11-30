068ea57ce46620fc216b9aad5b216be557ae78f1
jwx1282937 <jialinbo@huawei.com>
2023-11-30 03:05:38 +0000

    fix: parameter judge
    Signed-off-by: jwx1282937 <jialinbo@huawei.com>

--- ipc/native/src/core/source/iremote_broker.cpp
+++ ipc/native/src/core/source/iremote_broker.cpp
@@ -57,6 +57,10 @@ bool BrokerRegistration::Register(const std::u16string &descriptor, const Constr
     if (descriptor.empty()) {
         return false;
     }
+    if (object == nullptr) {
+        ZLOGE(LABEL, "the borker object is nullptr");
+        return false;
+    }
 
     std::lock_guard<std::mutex> lockGuard(creatorMutex_);
     auto it = creators_.find(descriptor);
--- ipc/native/src/jni/source/ohos_rpc_message_parcel.cpp
+++ ipc/native/src/jni/source/ohos_rpc_message_parcel.cpp
@@ -14,9 +14,11 @@
  */
 
 #include "ohos_rpc_message_parcel.h"
+
 #include <ashmem.h>
 #include <securec.h>
 #include <unistd.h>
+
 #include "ipc_debug.h"
 #include "ipc_file_descriptor.h"
 #include "jkit_utils.h"
@@ -63,12 +65,20 @@ private:
 MessageParcel *JavaOhosRpcMessageParcelGetNative(JNIEnv *env, jobject object)
 {
     ZLOGD(LABEL, "enter");
+    if (env == nullptr) {
+        ZLOGE(LABEL, "env is null, could not get native parcel");
+        return nullptr;
+    }
     jlong nativeObject = env->GetLongField(object, g_jMessageParcel.nativeObject);
     return reinterpret_cast<MessageParcel *>(nativeObject);
 }
 
 sptr<Ashmem> Java_ohos_rpc_Ashmem_getSptrAshmem(JNIEnv *env, jobject object, jlong id)
 {
+    if (env == nullptr) {
+        ZLOGE(LABEL, "env is null, could not get sptrAshmem");
+        return nullptr;
+    }
     if (id == 0) {
         return nullptr;
     }
@@ -111,6 +121,10 @@ static const JNINativeMethod sMethods[] = {
 int JavaOhosRpcMessageParcelRegisterNativeMethods(JNIEnv *env)
 {
     ZLOGD(LABEL, "enter");
+    if (env = nullptr) {
+        ZLOGE(LABEL, "env is null, could not register native methods");
+        return JNI_ERR;
+    }
     jclass klazz = (jclass)env->NewGlobalRef(env->FindClass("ohos/rpc/MessageParcel"));
     if (klazz == nullptr) {
         ZLOGE(LABEL, "could not find class for MessageParcel");
@@ -147,6 +161,10 @@ int JavaOhosRpcMessageParcelRegisterNativeMethods(JNIEnv *env)
 jboolean JNICALL Java_ohos_rpc_MessageParcel_nativeWriteRemoteObject(JNIEnv *env, jobject parcel, jobject object)
 {
     ZLOGD(LABEL, "enter");
+    if (env == nullptr) {
+        ZLOGE(LABEL, "env is null, could not write remote object");
+        return JNI_FALSE;
+    }
     MessageParcel *nativeParcel = JavaOhosRpcMessageParcelGetNative(env, parcel);
     if (nativeParcel == nullptr) {
         ZLOGE(LABEL, "could not get native parcel for marshalling");
@@ -170,6 +188,10 @@ jboolean JNICALL Java_ohos_rpc_MessageParcel_nativeWriteRemoteObject(JNIEnv *env
 jobject JNICALL Java_ohos_rpc_MessageParcel_nativeReadRemoteObject(JNIEnv *env, jobject object)
 {
     ZLOGD(LABEL, "enter");
+    if (env = nullptr) {
+        ZLOGE(LABEL, "env is null, could not read remote object");
+        return nullptr;
+    }
     MessageParcel *nativeParcel = JavaOhosRpcMessageParcelGetNative(env, object);
     if (nativeParcel == nullptr) {
         ZLOGE(LABEL, "could not get native parcel for unmarshalling");
@@ -247,6 +269,10 @@ jboolean JNICALL Java_ohos_rpc_MessageParcel_nativeWriteInterfaceToken(JNIEnv *e
     jint len)
 {
     ZLOGD(LABEL, "enter");
+    if (env == nullptr) {
+        ZLOGE(LABEL, "env is null, could not write interface token");
+        return JNI_FALSE;
+    }
     MessageParcel *nativeParcel = JavaOhosRpcMessageParcelGetNative(env, object);
     if (nativeParcel == nullptr) {
         ZLOGE(LABEL, "could not get native parcel for marshalling");
