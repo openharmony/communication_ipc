b0ecabf107bae22f683c2cc66562ac9acd85198e
dengyutao <dengyutao2@huawei.com>
2024-01-02 03:39:33 +0000

    update interfaces/innerkits/ipc_core/include/ipc_object_stub.h.
    Signed-off-by: dengyutao <dengyutao2@huawei.com>

--- interfaces/innerkits/ipc_core/include/ipc_object_stub.h
+++ interfaces/innerkits/ipc_core/include/ipc_object_stub.h
@@ -203,13 +203,6 @@ public:
      * @since 11
      */
     uint64_t GetLastRequestTime();
-
-    /**
-     * @brief Obtains string.
-     * @return Returns string.
-     * @since 11
-     */
-    std::string UIntToString(uint32_t input);
 #ifndef CONFIG_IPC_SINGLE
     /**
      * @brief Invoker the calling thread.
