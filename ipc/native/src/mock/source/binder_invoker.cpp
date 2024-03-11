276941d7c37667c9327cc1ad0821e1a5239a6d3c
luodonghui <luodonghui@huawei.com>
2024-03-11 17:23:15 +0800

    修复reply内存泄露问题
    Signed-off-by: luodonghui <luodonghui@huawei.com>
    Change-Id: Iada040a091298f5d446951789ec467cbc1f976c0

--- ipc/native/src/mock/source/binder_invoker.cpp
+++ ipc/native/src/mock/source/binder_invoker.cpp
@@ -690,25 +690,24 @@ int BinderInvoker::HandleReply(MessageParcel *reply)
         return status;
     }
 
-    if (tr->data_size > 0) {
-        auto allocator = new (std::nothrow) BinderAllocator();
-        if (allocator == nullptr) {
-            ZLOGE(LABEL, "create BinderAllocator object failed");
-            return IPC_INVOKER_INVALID_DATA_ERR;
-        }
-        if (!reply->SetAllocator(allocator)) {
-            ZLOGD(LABEL, "SetAllocator failed");
-            delete allocator;
-            FreeBuffer(reinterpret_cast<void *>(tr->data.ptr.buffer));
-            return IPC_INVOKER_INVALID_DATA_ERR;
-        }
-        reply->ParseFrom(tr->data.ptr.buffer, tr->data_size);
+    auto allocator = new (std::nothrow) BinderAllocator();
+    if (allocator == nullptr) {
+        ZLOGE(LABEL, "create BinderAllocator object failed");
+        return IPC_INVOKER_INVALID_DATA_ERR;
     }
+    if (!reply->SetAllocator(allocator)) {
+        ZLOGD(LABEL, "SetAllocator failed");
+        delete allocator;
+        FreeBuffer(reinterpret_cast<void *>(tr->data.ptr.buffer));
+        return IPC_INVOKER_INVALID_DATA_ERR;
+    }
+    reply->ParseFrom(tr->data.ptr.buffer, tr->data_size);
 
     if (tr->offsets_size > 0) {
         reply->InjectOffsets(tr->data.ptr.offsets, tr->offsets_size / sizeof(binder_size_t));
         reply->SetClearFdFlag();
     }
+
     return ERR_NONE;
 }
 
