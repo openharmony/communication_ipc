8877f9758b215f2019ebb30acf6360a4a3931dd5
xdx1921 <fengxu12@huawei.com>
2024-04-25 11:55:09 +0800

    UT修复与补充
    Signed-off-by: xdx1921 <fengxu12@huawei.com>

--- services/dbinder/test/unittest/dbinder_remote_listener_unittest.cpp
+++ services/dbinder/test/unittest/dbinder_remote_listener_unittest.cpp
@@ -17,7 +17,6 @@
 #include "gtest/gtest.h"
 #include "rpc_log.h"
 #include "log_tags.h"
-#include "session_impl.h"
 #define private public
 #include "dbinder_remote_listener.h"
 #undef private
@@ -32,6 +31,7 @@ public:
     static void TearDownTestCase(void);
     void SetUp();
     void TearDown();
+    const std::string NETWORKID_TEST = "123456789";
     static constexpr HiLogLabel LABEL = { LOG_CORE, LOG_ID_TEST, "DBinderRemoteListenerUnitTest" };
 };
 
@@ -48,4 +48,141 @@ HWTEST_F(DBinderRemoteListenerUnitTest, CreateClientSocket_001, TestSize.Level1)
     DBinderRemoteListener dBinderRemoteListener;
     std::string networkId = "";
     EXPECT_EQ(dBinderRemoteListener.CreateClientSocket(networkId), SOCKET_ID_INVALID);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, ServerOnBind_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    int32_t socketId = 1;
+    PeerSocketInfo info = {
+        .networkId = const_cast<char *>(NETWORKID_TEST.c_str()),
+    };
+
+    dBinderRemoteListener.ServerOnBind(socketId, info);
+    EXPECT_NE(dBinderRemoteListener.serverSocketInfos_.size(), 0);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, ServerOnShutdown_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    std::string networkId = "networkId";
+    int32_t socketId = 1;
+    dBinderRemoteListener.clientSocketInfos_[networkId] = socketId;
+    dBinderRemoteListener.ServerOnShutdown(socketId, SHUTDOWN_REASON_PEER);
+    EXPECT_EQ(dBinderRemoteListener.serverSocketInfos_.size(), 0);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, onbytesreceived_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const char* data = nullptr;
+    auto len = sizeof(struct DHandleEntryTxRx);
+    int32_t socketId = 1;
+    dBinderRemoteListener.OnBytesReceived(socketId, data, len);
+    EXPECT_EQ(data, nullptr);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, onbytesreceived_002, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const char *data = "testdatas";
+    ssize_t len = 0;
+    int32_t socketId = 1;
+    dBinderRemoteListener.OnBytesReceived(socketId, data, len);
+    EXPECT_EQ(len < static_cast<ssize_t>(sizeof(struct DHandleEntryTxRx)), true);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, onbytesreceived_003, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    auto len = sizeof(struct DHandleEntryTxRx);
+    dBinderRemoteListener.OnBytesReceived(1, nullptr, len);
+    EXPECT_EQ(dBinderRemoteListener.listenSocketId_, SOCKET_ID_INVALID);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, senddatatoremote_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string deviceId = "testdeviceid";
+    EXPECT_EQ(dBinderRemoteListener.SendDataToRemote(deviceId, nullptr), false);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, senddatatoremote_002, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string deviceId = "";
+    DHandleEntryTxRx message;
+    message.head.len = sizeof(DHandleEntryTxRx);
+    EXPECT_EQ(dBinderRemoteListener.SendDataToRemote(deviceId, &message), false);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, senddatatoremote_003, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string deviceId = "123";
+    DHandleEntryTxRx message;
+    message.head.len = sizeof(DHandleEntryTxRx);
+    EXPECT_EQ(dBinderRemoteListener.SendDataToRemote(deviceId, &message), false);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, startlistener_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    EXPECT_EQ(dBinderRemoteListener.StartListener(), false);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, StopListener_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    EXPECT_EQ(dBinderRemoteListener.StopListener(), true);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, closedatabussession_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string deviceId = "";
+    EXPECT_EQ(dBinderRemoteListener.ShutdownSocket(deviceId), false);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, QueryOrNewDeviceLock_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string deviceId = "";
+    dBinderRemoteListener.QueryOrNewDeviceLock(deviceId);
+    const std::string deviceId1 = "123456";
+    std::shared_ptr<DeviceLock> lockInfo = nullptr;
+    lockInfo = dBinderRemoteListener.QueryOrNewDeviceLock(deviceId1);
+    EXPECT_TRUE(lockInfo != nullptr);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, SendDataReply_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string deviceId = "";
+    EXPECT_EQ(dBinderRemoteListener.SendDataReply(deviceId, nullptr), false);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, SendDataReply_002, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string deviceId = "";
+    DHandleEntryTxRx message;
+    message.deviceIdInfo.fromDeviceId[0] = 't';
+    EXPECT_EQ(dBinderRemoteListener.SendDataReply(deviceId, &message), false);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, SendDataReply_003, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string deviceId = "12345";
+    DHandleEntryTxRx message;
+    message.deviceIdInfo.fromDeviceId[0] = 't';
+    EXPECT_EQ(dBinderRemoteListener.SendDataReply(deviceId, &message), false);
+}
+
+HWTEST_F(DBinderRemoteListenerUnitTest, OpenSoftbusSession_001, TestSize.Level1)
+{
+    DBinderRemoteListener dBinderRemoteListener;
+    const std::string peerDeviceId = "12345";
+    EXPECT_EQ(dBinderRemoteListener.CreateClientSocket(peerDeviceId), SOCKET_ID_INVALID);
 }
