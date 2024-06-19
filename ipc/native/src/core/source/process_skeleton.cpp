d13f9cef24993dd11ed65c21273f964f633ad08d
xdx1921 <fengxu12@huawei.com>
2024-06-19 09:55:42 +0000

    update ipc/native/src/core/source/process_skeleton.cpp.
    Signed-off-by: xdx1921 <fengxu12@huawei.com>

--- ipc/native/src/core/source/process_skeleton.cpp
+++ ipc/native/src/core/source/process_skeleton.cpp
@@ -295,11 +295,11 @@ bool ProcessSkeleton::DetachInvokerProcInfo(bool isLocal)
     std::string key = std::to_string(gettid()) + "_" + std::to_string(isLocal);
     auto it = invokerProcInfo_.find(key);
     if (it != invokerProcInfo_.end()) {
-        InvokerProcInfo.erase(it);
+        invokerProcInfo_.erase(it);
         return true;
     }
     ZLOGW(LOG_LABEL, "key %{public}s not found", key.c_str());
-    return false; 
+    return false;
 }
 
 bool ProcessSkeleton::IsPrint(int err, int &lastErr, int &lastErrCnt)
