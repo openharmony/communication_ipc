16056913dc88903668073af3755e3449af4635f1
Lixiaoying25 <lixiaoying25@huawei.com>
2024-07-31 17:09:23 +0800

    去除readByteArray中对数组长度40960的校验
    Signed-off-by: Lixiaoying25 <lixiaoying25@huawei.com>

--- ipc/native/src/napi_common/source/napi_message_sequence_read.cpp
+++ ipc/native/src/napi_common/source/napi_message_sequence_read.cpp
@@ -395,12 +395,7 @@ napi_value NAPI_MessageSequence::JS_readByteArray(napi_env env, napi_callback_in
         ZLOGE(LOG_LABEL, "napiSequence is null");
         return napiErr.ThrowError(env, errorDesc::READ_DATA_FROM_MESSAGE_SEQUENCE_ERROR);
     }
-    uint32_t maxBytesLen = MAX_BYTES_LENGTH;
     uint32_t arrayLength = napiSequence->nativeParcel_->ReadUint32();
-    if (arrayLength >= maxBytesLen) {
-        ZLOGE(LOG_LABEL, "byte array length too large");
-        return napiErr.ThrowError(env, errorDesc::CHECK_PARAM_ERROR);
-    }
 
     if (argc > 0) {
         napi_value argv[ARGV_LENGTH_1] = {0};
