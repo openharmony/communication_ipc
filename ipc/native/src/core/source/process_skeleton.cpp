21fa3c7b872b5db6f494fe2107ccd1cdc00c02b0
Lixiaoying25 <lixiaoying25@huawei.com>
2024-08-09 17:55:28 +0800

    删除指针
    Signed-off-by: Lixiaoying25 <lixiaoying25@huawei.com>

--- ipc/native/src/core/source/process_skeleton.cpp
+++ ipc/native/src/core/source/process_skeleton.cpp
@@ -270,17 +270,11 @@ bool ProcessSkeleton::AttachInvokerProcInfo(bool isLocal, InvokerProcInfo &invok
 {
     CHECK_INSTANCE_EXIT_WITH_RETVAL(exitFlag_, false);
     std::unique_lock<std::shared_mutex> lockGuard(invokerProcMutex_);
-    if (!invokeInfo.invoker) {
-        ZLOGE(LOG_LABEL, "invoker dead, %{public}u %{public}u %{public}u %{public}" PRIu64 " %{public}" PRIu64,
-            invokeInfo.pid, invokeInfo.realPid, invokeInfo.uid, invokeInfo.tokenId, invokeInfo.firstTokenId);
-        return false;
-    }
     std::string key = std::to_string(gettid()) + "_" + std::to_string(isLocal);
     auto result = invokerProcInfo_.insert_or_assign(key, invokeInfo);
     auto &info = result.first->second;
     ZLOGD(LOG_LABEL, "%{public}u, %{public}u %{public}u %{public}u %{public}" PRIu64 " %{public}" PRIu64,
-        ConvertAddr(reinterpret_cast<void*>(info.invoker)), info.pid, info.realPid,
-        info.uid, info.tokenId, info.firstTokenId);
+        info.invoker, info.pid, info.realPid, info.uid, info.tokenId, info.firstTokenId);
     return result.second;
 }
 
@@ -295,8 +289,8 @@ bool ProcessSkeleton::QueryInvokerProcInfo(bool isLocal, InvokerProcInfo &invoke
     }
     invokeInfo = it->second;
     ZLOGD(LOG_LABEL, "%{public}u, %{public}u %{public}u %{public}u %{public}" PRIu64 " %{public}" PRIu64,
-        ConvertAddr(reinterpret_cast<void*>(invokeInfo.invoker)), invokeInfo.pid, invokeInfo.realPid,
-        invokeInfo.uid, invokeInfo.tokenId, invokeInfo.firstTokenId);
+        invokeInfo.invoker, invokeInfo.pid, invokeInfo.realPid, invokeInfo.uid, invokeInfo.tokenId,
+        invokeInfo.firstTokenId);
     return true;
 }
 
@@ -309,8 +303,8 @@ bool ProcessSkeleton::DetachInvokerProcInfo(bool isLocal)
     if (it != invokerProcInfo_.end()) {
         auto &invokeInfo = it->second;
         ZLOGD(LOG_LABEL, "%{public}u, %{public}u %{public}u %{public}u %{public}" PRIu64 " %{public}" PRIu64,
-            ConvertAddr(reinterpret_cast<void*>(invokeInfo.invoker)), invokeInfo.pid, invokeInfo.realPid,
-            invokeInfo.uid, invokeInfo.tokenId, invokeInfo.firstTokenId);
+            invokeInfo.invoker, invokeInfo.pid, invokeInfo.realPid, invokeInfo.uid, invokeInfo.tokenId,
+            invokeInfo.firstTokenId);
         invokerProcInfo_.erase(it);
         return true;
     }
