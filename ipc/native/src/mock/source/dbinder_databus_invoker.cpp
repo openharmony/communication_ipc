fae5de8fbe5a4bbcf0b30d302ad52608cad75c68
hu-xiusong <huxiusong1@huawei.com>
2024-09-14 13:08:48 +0000

    fix:check num str
    Signed-off-by: hu-xiusong <huxiusong1@huawei.com>

--- ipc/native/src/mock/source/dbinder_databus_invoker.cpp
+++ ipc/native/src/mock/source/dbinder_databus_invoker.cpp
@@ -955,11 +955,24 @@ bool DBinderDatabusInvoker::SetCallingIdentity(std::string &identity, bool flag)
     if (identity.empty() || identity.length() <= DEVICEID_LENGTH) {
         return false;
     }
-
-    uint64_t tokenId = std::stoull(identity.substr(0, ACCESS_TOKEN_MAX_LEN).c_str());
-    std::string deviceId = identity.substr(ACCESS_TOKEN_MAX_LEN, DEVICEID_LENGTH);
-    uint64_t token = std::stoull(identity.substr(ACCESS_TOKEN_MAX_LEN + DEVICEID_LENGTH,
-        identity.length() - ACCESS_TOKEN_MAX_LEN - DEVICEID_LENGTH).c_str());
+    std::string tokenIdStr;
+    if (!ProcessSkeleton::GetSubStr(identity, tokenIdStr, 0, ACCESS_TOKEN_MAX_LEN) ||
+        !ProcessSkeleton::IsNumStr(tokenIdStr)) {
+        ZLOGE(LOG_LABEL, "Identity param tokenId is invalid, tokenIdStr:%{public}s", tokenIdStr.c_str());
+        return false;
+    }
+    std::string deviceId;
+    if (!ProcessSkeleton::GetSubStr(identity, deviceId, ACCESS_TOKEN_MAX_LEN, DEVICEID_LENGTH)) {
+        ZLOGE(LOG_LABEL, "Identity param deviceId is invalid, deviceId:%{public}s", deviceId.c_str());
+    }
+    std::string tokenStr;
+    if (!ProcessSkeleton::GetSubStr(identity, tokenStr, ACCESS_TOKEN_MAX_LEN + DEVICEID_LENGTH,
+        identity.length() - ACCESS_TOKEN_MAX_LEN - DEVICEID_LENGTH) || !ProcessSkeleton::IsNumStr(tokenStr)) {
+        ZLOGE(LOG_LABEL, "Identity param token is invalid, tokenStr:%{public}s", tokenStr.c_str());
+        return false;
+    }
+    uint64_t tokenId = std::stoull(tokenIdStr.c_str());
+    uint64_t token = std::stoull(tokenStr.c_str());
 
     callerUid_ = static_cast<int>(token >> PID_LEN);
     callerPid_ = static_cast<int>(token);
