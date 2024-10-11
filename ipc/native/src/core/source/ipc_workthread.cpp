380b71e61423d0ebdd4db589d3a7caf709f6bf25
wangwang <wangwang16@huawei.com>
2024-10-11 06:18:53 +0000

    update ipc/native/src/core/source/ipc_workthread.cpp.
    Signed-off-by: wangwang <wangwang16@huawei.com>

--- ipc/native/src/core/source/ipc_workthread.cpp
+++ ipc/native/src/core/source/ipc_workthread.cpp
@@ -108,7 +108,7 @@ void *IPCWorkThread::ThreadHandler(void *args)
         ZLOGE(LOG_LABEL, "set thread name:%{public}s fail, ret:%{public}d", threadName.c_str(), ret);
     } else {
         IRemoteInvoker *invoker = IPCThreadSkeleton::GetRemoteInvoker(param->proto);
-        ZLOGI(LOG_LABEL, "proto:%{public}d policy:%{public}d name:%{public}s invoker:%{public}u ",
+        ZLOGI(LOG_LABEL, "proto:%{public}d policy:%{public}d name:%{public}s invoker:%{public}u",
             param->proto, param->policy, threadName.c_str(), ProcessSkeleton::ConvertAddr(invoker));
     }
     IPCThreadSkeleton::SaveThreadName(threadName);
