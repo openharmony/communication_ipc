cb52784bbca1e4f73e612cf8e84ed91f92e48611
chenchong_666 <chenchong57@huawei.com>
2024-10-28 21:36:06 +0800

    test: add IPCDbinderDataBusInvokerTest
    Signed-off-by: chenchong_666 <chenchong57@huawei.com>

---
+++ ipc/native/test/unittest/common/mock_ipc_process_skeleton.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_MOCK_IPC_PROCESS_SKELETON_H
+#define OHOS_MOCK_IPC_PROCESS_SKELETON_H
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "ipc_process_skeleton.h"
+
+namespace OHOS {
+class MockIPCProcessSkeleton : public IPCProcessSkeleton {
+public:
+    MockIPCProcessSkeleton() {}
+    MOCK_METHOD0(GetCurrent, IPCProcessSkeleton* ());
+    MOCK_METHOD2(FindOrNewObject, sptr<IRemoteObject>(
+        int handle, const dbinder_negotiation_data *data));
+};
+} // namespace OHOS
+#endif // OHOS_MOCK_IPC_PROCESS_SKELETON_H
