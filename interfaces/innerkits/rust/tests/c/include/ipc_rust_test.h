e24503bbeb62b040b79dae134f2eb38e60a1248b
yueyan <yueyan8@huawei.com>
2025-04-07 10:01:03 +0800

    fix:processing string not in utf-8 format
    Signed-off-by: yueyan <yueyan8@huawei.com>

--- interfaces/innerkits/rust/tests/c/include/ipc_rust_test.h
+++ interfaces/innerkits/rust/tests/c/include/ipc_rust_test.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -39,6 +39,7 @@ public:
 extern "C" {
 MessageParcel *GetTestMessageParcel();
 MessageParcel *ReadAndWrite(MessageParcel &data);
+MessageParcel *GetMessageParcelContainingInvalidString();
 CStringWrapper *GetCallingDeviceID();
 uint64_t GetCallingFullTokenID();
 uint64_t GetCallingPid();
