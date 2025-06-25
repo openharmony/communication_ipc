4e878ac4f49441b64879d38dec875c04e030bf46
dongyang <dongyang22@huawei.com>
2025-06-25 11:59:51 +0800

    add_fuzz
    Signed-off-by: dongyang <dongyang22@huawei.com>

---
+++ test/fuzztest/dbinder/dbinder_service/src/getlocaldeviceid_fuzzer/getlocaldeviceid_fuzzer.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getlocaldeviceid_fuzzer.h"
+
+#include <dbinder_service.h>
+
+namespace OHOS {
+
+void GetLocalDeviceIDFuzzTest(const uint8_t *data, size_t size)
+{
+    if (data == nullptr || size == 0) {
+        return;
+    }
+
+    sptr<DBinderService> dBinderService = DBinderService::GetInstance();
+    (void)dBinderService->GetLocalDeviceID();
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::GetLocalDeviceIDFuzzTest(data, size);
+    return 0;
+}
