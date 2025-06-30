7e6e1e8a61d705afbb9e7144d46e29c283b8f5cd
hanlin15 <lihanlin15@huawei.com>
2025-06-30 09:36:42 +0800

    arkts1.2_head
    Signed-off-by: hanlin15 <lihanlin15@huawei.com>
    Change-Id: I782ace18b4cf9227ee51b6b87c247ae99b6d19ec

---
+++ ipc/native/src/taihe/inc/taihe_remote_proxy.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_IPC_TAIHE_REMOTEPROXY_H
+#define OHOS_IPC_TAIHE_REMOTEPROXY_H
+
+#include "ohos.rpc.rpc.proj.hpp"
+#include "ohos.rpc.rpc.impl.hpp"
+#include "taihe/runtime.hpp"
+#include "taihe/map.hpp"
+#include "stdexcept"
+
+#include <cinttypes>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+#include "ipc_object_proxy.h"
+
+namespace OHOS {
+
+class RemoteProxyImpl {
+public:
+    RemoteProxyImpl(uintptr_t nativePtr);
+
+    ::ohos::rpc::rpc::IRemoteBroker GetLocalInterface(::taihe::string_view descriptor);
+
+    ::ohos::rpc::rpc::RequestResult SendMessageRequestSync(int32_t code, ::ohos::rpc::rpc::weak::MessageSequence data,
+        ::ohos::rpc::rpc::weak::MessageSequence reply, ::ohos::rpc::rpc::weak::MessageOption options);
+
+    void RegisterDeathRecipient(::ohos::rpc::rpc::weak::DeathRecipient recipient, int32_t flags);
+
+    void UnregisterDeathRecipient(::ohos::rpc::rpc::weak::DeathRecipient recipient, int32_t flags);
+
+    ::taihe::string GetDescriptor();
+
+    bool IsObjectDead();
+
+    int64_t GetNativePtr();
+
+    void AddJsObjWeakRef(::ohos::rpc::rpc::weak::RemoteProxy obj);
+
+    static ::ohos::rpc::rpc::RemoteProxy CreateRemoteProxyFromNative(uintptr_t nativePtr);
+
+private:
+    OHOS::sptr<OHOS::IPCObjectProxy> cachedObject_;
+    std::optional<::ohos::rpc::rpc::RemoteProxy> jsObjRef_;
+    std::optional<::ohos::rpc::rpc::IRemoteBroker> jsLocalInterface_;
+    ::taihe::map<::ohos::rpc::rpc::DeathRecipient, OHOS::sptr<DeathRecipientImpl>> deathRecipientMap_;
+};
+
+} // namespace
+#endif // OHOS_IPC_TAIHE_REMOTEPROXY_H
