3eb39ef8a67ba92003f1b56feb5428b05ba2523d
hanlin15 <lihanlin15@huawei.com>
2025-06-30 09:36:42 +0800

    arkts1.2_head
    Signed-off-by: hanlin15 <lihanlin15@huawei.com>
    Change-Id: I782ace18b4cf9227ee51b6b87c247ae99b6d19ec

---
+++ ipc/native/src/taihe/inc/taihe_deathrecipient.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_IPC_TAIHE_DEATHRECIPIENT_H
+#define OHOS_IPC_TAIHE_DEATHRECIPIENT_H
+
+#include "ohos.rpc.rpc.proj.hpp"
+#include "ohos.rpc.rpc.impl.hpp"
+#include "taihe/runtime.hpp"
+#include "stdexcept"
+
+#include <cinttypes>
+#include <set>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+#include "iremote_object.h"
+
+namespace OHOS {
+class DeathRecipientImpl : public OHOS::IRemoteObject::DeathRecipient {
+public:
+    DeathRecipientImpl(::ohos::rpc::rpc::DeathRecipient jsObjRef);
+
+    void OnRemoteDied(const OHOS::wptr<OHOS::IRemoteObject> &object) override;
+
+    int64_t GetNativePtr();
+
+    void AddJsObjWeakRef(::ohos::rpc::rpc::weak::DeathRecipient obj);
+
+    static ::ohos::rpc::rpc::DeathRecipient CreateDeathRecipient();
+
+    ::ohos::rpc::rpc::DeathRecipient jsObjRef_;
+};
+} // namespace
+
+#endif // OHOS_IPC_TAIHE_DEATHRECIPIENT_H
