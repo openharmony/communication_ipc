1ad95fcfe96177f3f2a55d413aca471d97cb3a3c
Lixiaoying25 <lixiaoying25@h-partners.com>
2025-07-10 19:33:18 +0800

    日志朝鲜整改&编译告警整改
    Signed-off-by: Lixiaoying25 <lixiaoying25@h-partners.com>

--- ipc/native/src/core/invoker/source/binder_invoker.cpp
+++ ipc/native/src/core/invoker/source/binder_invoker.cpp
@@ -450,7 +450,7 @@ bool BinderInvoker::UnFlattenDBinderObject(Parcel &parcel, dbinder_negotiation_d
 bool BinderInvoker::SetMaxWorkThread(int maxThreadNum)
 {
     if ((binderConnector_ == nullptr) || (!binderConnector_->IsDriverAlive())) {
-        ZLOGE(LABEL, "driver died");
+        ZLOGE(LABEL, "died");
         return false;
     }
 
@@ -466,7 +466,7 @@ bool BinderInvoker::SetMaxWorkThread(int maxThreadNum)
 int BinderInvoker::FlushCommands(IRemoteObject *object)
 {
     if ((binderConnector_ == nullptr) || (!binderConnector_->IsDriverAlive())) {
-        ZLOGE(LABEL, "driver is died");
+        ZLOGE(LABEL, "died");
         return IPC_INVOKER_CONNECT_ERR;
     }
     int error = TransactWithDriver(false);
@@ -497,7 +497,7 @@ int BinderInvoker::FlushCommands(IRemoteObject *object)
 void BinderInvoker::ExitCurrentThread()
 {
     if ((binderConnector_ == nullptr) || (!binderConnector_->IsDriverAlive())) {
-        ZLOGE(LABEL, "driver died when exit current thread");
+        ZLOGE(LABEL, "died");
         return;
     }
     binderConnector_->ExitCurrentThread(BINDER_THREAD_EXIT);
@@ -507,7 +507,7 @@ void BinderInvoker::ExitCurrentThread()
 void BinderInvoker::StartWorkLoop()
 {
     if ((binderConnector_ == nullptr) || (!binderConnector_->IsDriverAlive())) {
-        ZLOGE(LABEL, "driver is died");
+        ZLOGE(LABEL, "died");
         return;
     }
     int error;
@@ -538,7 +538,7 @@ void BinderInvoker::StartWorkLoop()
             isFirstInvoke_ = STATUS_NOT_FIRST_INVOKE;
         }
         if ((userError == -ERR_TIMED_OUT || userError == IPC_INVOKER_INVALID_DATA_ERR) && !isMainWorkThread) {
-            ZLOGW(LABEL, "exit, userError:%{public}d", userError);
+            ZLOGW(LABEL, "exit, error:%{public}d", userError);
             break;
         }
         ProcDeferredDecRefs();
@@ -1240,7 +1240,7 @@ void BinderInvoker::DealWithCmd(MessageParcel *reply, bool &continueLoop, int32_
 int BinderInvoker::WaitForCompletion(MessageParcel *reply)
 {
     if ((binderConnector_ == nullptr) || (!binderConnector_->IsDriverAlive())) {
-        ZLOGE(LABEL, "driver is died");
+        ZLOGE(LABEL, "died");
         return IPC_INVOKER_CONNECT_ERR;
     }
     uint32_t cmd;
@@ -1653,7 +1653,7 @@ bool BinderInvoker::SetCallingIdentity(std::string &identity, bool flag)
 bool BinderInvoker::TriggerSystemIPCThreadReclaim()
 {
     if ((binderConnector_ == nullptr) || (!binderConnector_->IsDriverAlive())) {
-        ZLOGE(LABEL, "driver died");
+        ZLOGE(LABEL, "died");
         return false;
     }
 
@@ -1670,7 +1670,7 @@ bool BinderInvoker::TriggerSystemIPCThreadReclaim()
 bool BinderInvoker::EnableIPCThreadReclaim(bool enable)
 {
     if ((binderConnector_ == nullptr) || (!binderConnector_->IsDriverAlive())) {
-        ZLOGE(LABEL, "driver died");
+        ZLOGE(LABEL, "died");
         return false;
     }
 
