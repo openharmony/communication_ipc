19a6121340012644d2bf459d405847097e6615bb
hanlin15 <lihanlin15@huawei.com>
2025-08-28 16:20:45 +0800

    fix ut
    Signed-off-by: hanlin15 <lihanlin15@huawei.com>

--- test/unittest/dbinder/dbinder_service/src/dbinder_service_unittest.cpp
+++ test/unittest/dbinder/dbinder_service/src/dbinder_service_unittest.cpp
@@ -455,6 +455,7 @@ HWTEST_F(DBinderServiceTest, InvokerRemoteDBinderTest003, TestSize.Level1)
     std::shared_ptr<struct ThreadLockInfo> threadLockInfo = std::make_shared<struct ThreadLockInfo>();
     dBinderService.remoteListener_ = std::make_shared<DBinderRemoteListener>();
     dBinderService.AttachThreadLockInfo(PID, RANDOM_DEVICEID, threadLockInfo);
+
     int32_t result = dBinderService.InvokerRemoteDBinder(dBinderServiceStub, PID, PID, PID);
     EXPECT_EQ(result, DBinderErrorCode::MAKE_THREADLOCK_FAILED);
 }
